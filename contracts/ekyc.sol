3581b2a17a79e7e27b0134dddc532456963bbc7f
Remix Workspace <>
2022-08-09 20:02:39 +0530

    Bank Program

--- contracts/ekyc.sol
+++ contracts/ekyc.sol
@@ -6,6 +6,16 @@ uint16 Flat_No;
 string Flat_Owner;
 
 mapping(uint16 => string) MyAssosiation;
+function stringsEquals(string memory s1, string memory s2) public pure returns (bool) { 
+        bytes memory b1 = bytes(s1); 
+        bytes memory b2 = bytes(s2); 
+        uint256 l1 = b1.length; 
+        if (l1 != b2.length) return false; 
+        for (uint256 i = 0; i < l1; i++) { 
+            if (b1[i] != b2[i]) return false; 
+        } 
+        return true; 
+} 
 
 function setassosiationdata(uint16 _Flat_No, string memory _Flat_Owner) public {
     MyAssosiation[_Flat_No]=_Flat_Owner;
--- contracts/kyc.sol
+++ contracts/kyc.sol
@@ -3,6 +3,18 @@
 pragma solidity > 0.6.1;
 
 contract kyc{
+
+address kycadmin;
+
+constructor() public{
+    kycadmin = msg.sender;
+}
+
+modifier OnlyAdmin() {
+    require(msg.sender==kycadmin, "User is not admin");
+    _;
+}
+
 struct Bank{
     string BankName;
     address BankAddress;
@@ -21,38 +33,61 @@ struct Customer{
 mapping (address => Bank) Banks;
 mapping (string => Customer) BankCustomers;
 
-function AddNewBank(string memory _BankName, address _BankAddress ) public {
+
+function AddNewBank(string memory _BankName, address _BankAddress ) public OnlyAdmin {
+    //require(Banks[_BankAddress].BankAddress!= address(0), "Bank already exists!");
     Banks[_BankAddress] = Bank(_BankName, _BankAddress, 0, true, true);
 }
 
-
-function AllowBankToDoKYC(address _BankAddress) public {
-    Banks[_BankAddress].KYCPrivilage =true;
-}
-
-function BlockBankToDoKYC(address _BankAddress) public {
-    Banks[_BankAddress].KYCPrivilage =false;
-}
-
-function AllowBankToAddCustomer(address _BankAddress) public {
-    Banks[_BankAddress].CanAddCustomer =true;
-}
-
-function BlockBankToAddCustomer(address _BankAddress) public {
-    Banks[_BankAddress].CanAddCustomer =true;
+function GetBankDetails(address _BankAddress) public view returns(string memory, address,uint256, bool, bool ) {
+    require(Banks[_BankAddress].BankAddress!= address(0), "Bank not found!");
+    return(Banks[_BankAddress].BankName,Banks[_BankAddress].BankAddress,Banks[_BankAddress].KYCCount ,Banks[_BankAddress].CanAddCustomer, Banks[_BankAddress].KYCPrivilage );
 }
 
 function AddNewCustomer(string memory _CustomerName, string memory _CustomerData, address _CustomerBank ) public {
-    require(Banks[_CustomerBank].CanAddCustomer, "Does not have permission to add new customers");
+    require(Banks[_CustomerBank].BankAddress!= address(0), "Bank not found!");
+    require(Banks[_CustomerBank].CanAddCustomer, "Does not have permission to add new customers!");
     BankCustomers[_CustomerName] = Customer(_CustomerName, _CustomerData, _CustomerBank, false);
 }
 
+function CheckKYCStatus(string memory _CustomerName)public view returns(bool){
+    require(BankCustomers[_CustomerName].CustomerBank!= address(0), "Customer does not exists!");
+    return(BankCustomers[_CustomerName].KYCStatus);
+}
+
 function AddNewCustomerRequestforKYC(string memory _CustomerName, address _CustomerBank)public {
-    require(Banks[_CustomerBank].KYCPrivilage, "Does not have permission to do KYC");
+    require(Banks[_CustomerBank].BankAddress!= address(0), "Bank not found!");
+    require(Banks[_CustomerBank].KYCPrivilage, "Does not have permission to do KYC!");
+    Banks[_CustomerBank].KYCCount++;
     BankCustomers[_CustomerName].KYCStatus = true;
 }
 
+
+function BlockBankToAddCustomer(address _BankAddress) public OnlyAdmin{
+    require(Banks[_BankAddress].BankAddress!= address(0), "Bank not found!");
+    Banks[_BankAddress].CanAddCustomer =false;
+}
+
+
+function BlockBankToDoKYC(address _BankAddress) public OnlyAdmin{
+    require(Banks[_BankAddress].BankAddress!= address(0), "Bank not found!");
+    Banks[_BankAddress].KYCPrivilage =false;
+}
+
+function AllowBankToAddCustomer(address _BankAddress) public OnlyAdmin{
+    require(Banks[_BankAddress].BankAddress!= address(0), "Bank not found!");
+    Banks[_BankAddress].CanAddCustomer =true;
+}
+
+
+function AllowBankToDoKYC(address _BankAddress) public OnlyAdmin {
+    require(Banks[_BankAddress].BankAddress!= address(0), "Bank not found!");
+    Banks[_BankAddress].KYCPrivilage =true;
+}
+
+
 function ViewCustomerData(string memory _CustomerName) public view returns(string memory, address, bool){
+    require(BankCustomers[_CustomerName].CustomerBank!= address(0), "Customer not found!");
     return(BankCustomers[_CustomerName].CustomerData, BankCustomers[_CustomerName].CustomerBank, BankCustomers[_CustomerName].KYCStatus);
 }
 
